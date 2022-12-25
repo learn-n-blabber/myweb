1fb9138831d1a0e9330f01bf06665d931d15b17e
learn-n-blabber <98074151+learn-n-blabber@users.noreply.github.com>
2022-12-25 12:23:48 +0530

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+/* global document*/
+function(){
+  var torch = document.getElementsByClassName("torch")[0];
+  torch.style.clipPath = 'circle(80px at ${event.offsetX}px ${event.offsetY}px)';
+}
+
+
+
